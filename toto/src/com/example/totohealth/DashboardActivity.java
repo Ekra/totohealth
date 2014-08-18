1270555f4429fa5ac87655138078c7921847e734
ekra <ekranyambura@gmail.com>
2014-08-18 19:14:39 +0300

    totohealth login

---
+++ toto/src/com/example/totohealth/DashboardActivity.java
@@ -0,0 +1,22 @@
+package com.example.totohealth;
+
+
+
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import library.UserFunctions;
+ 
+public class DashboardActivity extends Activity {
+    UserFunctions userFunctions;
+    Button btnLogout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+           
+    }
+}
